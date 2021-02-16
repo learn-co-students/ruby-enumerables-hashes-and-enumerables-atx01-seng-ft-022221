ac9ef610e99bbba683691cef35c212d9c1ca7585
Robert Obi <robobi97@googlemail.com>
2021-02-16 18:24:29 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,19 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+ passengers = {
+ suite_a: "Amanda Presley", 
+ suite_b: "Seymour Hoffman", 
+ suite_c: "Alfred Tennyson", 
+ suite_d: "Charlie Chaplin", 
+ suite_e: "Crumpet the Elf"
+ }
 
 def select_winner(passengers)
-  # add the code snippet here!
-end
+  winner = ""
+  passengers.each do |key, value|
+    if key == :suite_a && value[0][0] == "A"
+      winner = winner + value
+    end
+ end 
+ p winner
+end 
+
+
